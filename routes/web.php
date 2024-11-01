682a788437400a06001ea456808a1e49ca7adece
mohamadreza <mohamadreza1388.org@gmail.com>
2024-11-01 09:26:33 +0330

    feat(test)

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class);
-
+//test
 Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
     Route::get('/clear', function () {
         Artisan::call('cache:clear');
